7c59aa3f7c1963b9c820369cbc9536a6651cd449
Mayur Nikam <mayur.nikam@ncr.com>
2022-06-20 14:49:39 -0700

    Updated the Home Screen title

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "React Next Gen App",
+      title: "React Next Gen App By Mayur",
     },
   }
 );
